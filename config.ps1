d6015a85705224d6443cabd2ac88410fdd5caf7f
Bernd Lörwald <bloerwald@googlemail.com>
2018-01-27 21:59:57 +0100

    powershell scripts to run rendersrevice with known and autogenerated configs

---
+++ config.ps1
@@ -0,0 +1,7 @@
+﻿$install_dir = "E:\wow8_beta"
+
+$scenes_dir = "$script_dir\Scenes"
+$port = 12346
+$log_dir = "$install_dir\Logs"
+$log_level = 100
+$product = 'product="wow_beta" patch="80001" build="25902"'
---
+++ run_renderservice.ps1
@@ -0,0 +1,20 @@
+﻿$script_dir = (split-path $MyInvocation.MyCommand.Path) 
+. $script_dir/config.ps1
+
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+
+$tmp = New-TemporaryFile
+$tmppath = $tmp.FullName
+
+Write "<?xml version=`"1.0`" encoding=`"UTF-8`" standalone=`"no`" ?>" | Out-File "$tmppath"
+Write "<config>" | Out-File -Append "$tmppath"
+Write "  <scenes path=`"$scenes_dir`" />" | Out-File -Append "$tmppath"
+Write "  <log path=`"$log_dir`" level=`"$log_level`" />" | Out-File -Append "$tmppath"
+Write "  <cas $product />" | Out-File -Append "$tmppath"
+Write "  <thread low-priority-count=`"1`" />" | Out-File -Append "$tmppath"
+Write "  <bnet-api path=`"bnet.marlam.in/fake-bnet-api`" timeout=`"5`" max-connection=`"10`" />" | Out-File -Append "$tmppath"
+Write "</config>" | Out-File -Append "$tmppath"
+
+cat $tmppath
+
+& "$install_dir\RenderService-64.exe" -port $port -config $tmppath
