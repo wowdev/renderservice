ecb5e6735cd41d0142a76c0937190c8de850db58
Bernd Lörwald <bloerwald@googlemail.com>
2018-01-28 18:52:46 +0100

    idb: use less horrible format for storage

--- idb-export.py
+++ idb-export.py
@@ -11,6 +11,7 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 for position, name in Names():
 	names[position] = name
 
-with open(script_dir + "/idb-" + str(ida_nalt.retrieve_input_file_crc32()) + ".pickle", "w") as file:
-	pickle.dump (names, file)
-print("pickled names")
+with open(script_dir + "/idb-" + str(ida_nalt.retrieve_input_file_crc32()) + ".names", "w") as file:
+	for position, name in names.iteritems():
+		file.write("{} {}\n".format (position, name))
+print("wrote names")
--- idb-import.py
+++ idb-import.py
@@ -8,9 +8,11 @@ names = {}
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
-with open(script_dir + "/idb-" + str(ida_nalt.retrieve_input_file_crc32()) + ".pickle", "r") as file:
-	names = pickle.load (file)
+with open(script_dir + "/idb-" + str(ida_nalt.retrieve_input_file_crc32()) + ".names", "r") as file:
+	for line in file.read().splitlines():
+		s = line.split(" ")
+		names[int(s[0])] = " ".join (s[1:])
 	
 for position, name in names.iteritems():
 	idc.MakeName(position, name)
-print("unpickled names")
+print("read names")
