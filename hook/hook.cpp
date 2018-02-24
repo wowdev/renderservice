ad373256bd504a79cf751c077c108f2059b0f6ec
Bernd Lörwald <bloerwald@googlemail.com>
2018-02-25 00:42:41 +0100

    26095: avoid unknown dbs

--- hook/hook.cpp
+++ hook/hook.cpp
@@ -26,8 +26,45 @@ struct callback_test_t
 };
 blz::static_function<callback_test_t, void, RenderRequest::RequestData*, route_arg_3_t*> const callback_test;
 
+struct wowdbclient {
+  virtual ~wowdbclient() = 0;
+  struct info_t {
+    char const* name;
+  };
+  info_t* info;
+};
+//! \note hardcoded to 8.0.1.26095
+fun<bool (wowdbclient*, char const*, char const*, unsigned int)> db_load_with_dbgenfallback
+  = 0x1401F7A30;
+fun<bool (wowdbclient*, unsigned int)> db_load_with_dbgenfallback_no_override
+  = 0x1401F7B20;
+
 extern "C" void __declspec(dllexport) __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO* inRemoteInfo)
 {
+  hook ( db_load_with_dbgenfallback
+       , static_cast<decltype(db_load_with_dbgenfallback)::signature*>([] (wowdbclient* a, char const* b, char const* c, unsigned int d)
+         {
+           std::cerr << "load db2 " << a->info->name << "\n";
+           if (std::string (a->info->name) == "AnimationNames") return true;
+           if (std::string (a->info->name) == "FileDataComplete") return true;
+           if (std::string (a->info->name) == "FilePaths") return true;
+           return db_load_with_dbgenfallback (a, b, c, d);
+         }
+         )
+       );
+  hook ( db_load_with_dbgenfallback_no_override
+       , static_cast<decltype(db_load_with_dbgenfallback_no_override)::signature*> ([] (wowdbclient* a, unsigned int b)
+         {
+           std::cerr << "load db2 " << a->info->name << "\n";
+           if (std::string (a->info->name) == "AnimationNames") return true;
+           if (std::string (a->info->name) == "FileDataComplete") return true;
+           if (std::string (a->info->name) == "FilePaths") return true;
+           return db_load_with_dbgenfallback_no_override (a, b);
+         }
+         )
+       );
+
+
   hook ( CVarRegister
        , static_cast<CVar* (*)(char const*, char const*, unsigned int, char const*, bool (*)(CVar*, char const*, char const*, void *), unsigned int, bool, void *, bool)>
            ( [] (char const* name, char const* a, unsigned int b, char const* c, bool (*d)(CVar*, char const*, char const*, void *), unsigned int e, bool f, void * g, bool h)
