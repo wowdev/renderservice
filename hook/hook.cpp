f2a5049821b15d0382d0af6451488f2842e75263
Bernd Lörwald <bloerwald@googlemail.com>
2018-02-26 01:07:20 +0100

    hook: specific 8.0.1 and dbmeta dump

--- hook/hook.cpp
+++ hook/hook.cpp
@@ -9,6 +9,9 @@ BOOL APIENTRY DllMain (HMODULE, DWORD, LPVOID) { return TRUE; }
 #include <thread>
 #include <chrono>
 #include <unordered_map>
+#include <fstream>
+#include <map>
+#include <sstream>
 
 #include "asm_x64.hpp"
 #include "patching.hpp"
@@ -26,11 +29,52 @@ struct callback_test_t
 };
 blz::static_function<callback_test_t, void, RenderRequest::RequestData*, route_arg_3_t*> const callback_test;
 
+#pragma pack(push, 1)
+using _BYTE = unsigned char;
+struct struc_6
+{
+  const char *name;
+  int num_fields_in_file;
+  int record_size;
+  int num_fields;
+  int id_column;
+  char field_18;
+  _BYTE gap19[7];
+  int *field_offsets;
+  int *field_sizes;
+  int *field_types;
+  int *field_flags;
+  int *field_sizes_in_file;
+  int *field_types_in_file;
+  int *field_flags_in_file;
+  int field_58;
+  int table_hash;
+  _BYTE gap60[4];
+  int layout_hash;
+  int field_68;
+  int field_6C;
+  int field_70;
+  _BYTE gap74[4];
+  _UNKNOWN *field_78;
+  _BYTE gap80[8];
+  int field_88;
+  int field_8C;
+  int some_special_column;
+  _BYTE gap94[4];
+  __int64 sort_func;
+  const char *table_name;
+  const char **field_names_in_file;
+  const char **field_names;
+  const char *fk_clause;
+  char field_C0;
+  char field_C1;
+};
+#pragma pack(pop)
+static_assert(sizeof(struc_6) == 0xc2, "");
+
 struct wowdbclient {
-  virtual ~wowdbclient() = 0;
-  struct info_t {
-    char const* name;
-  };
+  void* vtable;
+  using info_t = struc_6;
   info_t* info;
 };
 //! \note hardcoded to 8.0.1.26095
@@ -38,62 +82,191 @@ fun<bool (wowdbclient*, char const*, char const*, unsigned int)> db_load_with_db
   = 0x1401F7A30;
 fun<bool (wowdbclient*, unsigned int)> db_load_with_dbgenfallback_no_override
   = 0x1401F7B20;
+fun<wowdbclient* (wowdbclient*, wowdbclient::info_t*)> sub_1401F3480 = 0x1401F3480;
+fun<bool()> renderservice_main = 0x14005D670;
 
-extern "C" void __declspec(dllexport) __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO* inRemoteInfo)
+std::string ea(void const* addr)
 {
+  auto unrebased = unrebase (addr);
+  std::stringstream ss; ss << "0x" << std::hex << unrebased;
+  return ss.str();
+}
+
+std::string make_type(void const* addr, std::string type)
+{
+  return "MakeUnkn(" + ea(addr) + ", 1);SetType(" + ea(addr) + ", \"" + type + "\");";
+}
+
+std::string type_to_str (int t, int f)
+try
+{
+  switch (t)
+  {
+    case 0:
+    case 1:
+    case 4:
+    case 5:
+      switch (f)
+      {
+        case 0|0|0:
+          return "int";   
+        case 0|0|1:
+          return "int?01"; // ???      
+        case 0|2|0: 
+          return "int?20"; // ???
+        case 0|2|1:
+          return "int?21"; // ???
+        case 4|0|0:
+          return "uint";
+        case 4|0|1:
+          return "uint?01"; // ???
+        case 4|2|0: 
+          return "uint?20"; // ???
+        case 4|2|1:
+          return "uint?21"; // ???
+        default:
+          throw std::logic_error ("f != ???");
+      }
+    case 2:
+      switch (f)
+      {
+        case 0:
+          return "string";
+        case 8:
+          return "locstring";
+        default:
+          throw std::logic_error ("f != 0/8");
+      }
+    case 3:
+      switch (f)
+      {
+        case 0:
+          return "float";
+        case 2:
+          return "float?2"; // ???
+        default:
+          throw std::logic_error ("f != 0");
+      }
+    default:
+      throw std::logic_error ("unknown type");
+  }
+}
+catch (std::exception const& ex)
+{
+  std::cerr << "type_to_str (" << t << ", " << f << ") = " << ex.what() << "\n";
+  throw;
+}
+
+void dump_db_info_ida (wowdbclient* a)
+{
+  std::string name = a->info->name;
+  std::string ida_cmd;
+  ida_cmd += "MakeName(" + ea (a) + ", \"db_" + a->info->name + "\");";
+  ida_cmd += "MakeName(" + ea (a->info) + ", \"__ZN" + std::to_string(name.size()) + name + "6s_metaE\");";
+  ida_cmd += make_type (a->info, "struc_6");
+  ida_cmd += make_type (a->info->table_name, "char const a[]");
+  ida_cmd += make_type (a->info->field_names_in_file, "char const* a[" + std::to_string (a->info->num_fields_in_file) + "]");
+  ida_cmd += make_type (a->info->field_names, "char const* a[" + std::to_string (a->info->num_fields) + "]");
+  std::ofstream("E:/git/renderserver/tmp.ida.py", std::ios_base::app) << ida_cmd << "\n";
+  
+  std::ostringstream dbdss;
+  dbdss << "COLUMNS\n";
+  
+  std::map<std::string, std::pair<int /*type*/, int /*flags*/>> names;
+  
+  for (int i = 0; i < a->info->num_fields_in_file; ++i) {
+    auto p = std::make_pair (a->info->field_types_in_file[i], a->info->field_flags_in_file[i]);
+    if ( !names.emplace (a->info->field_names_in_file[i], p).second
+      && names.at (a->info->field_names_in_file[i]) != p
+        )
+      throw std::logic_error ("different type for column in file and memory");
+  }
+  for (int i = 0; i < a->info->num_fields; ++i) {
+    auto p = std::make_pair (a->info->field_types[i], a->info->field_flags[i]);
+    if ( !names.emplace (a->info->field_names[i], p).second
+      && names.at (a->info->field_names[i]) != p
+        )
+      throw std::logic_error ("different type for column in file and memory");
+  }
+  
+  
+
+  for (auto const& name : names)
+    dbdss << type_to_str(name.second.first, name.second.second) << " " << name.first << "\n";
+  std::ofstream("E:/git/renderserver/tmp-" + name + ".dbd") << dbdss.str() << "\n";
+}
+
+wowdbclient *__fastcall sub_1401F3480_hook(wowdbclient *db, wowdbclient::info_t *info)
+{
+  //std::cerr << "ctor " << info->name << "\n";
+  sub_1401F3480 (db, info);
+  dump_db_info_ida (db);
+  return db;
+}
+
+void on_inject()
+{
+  std::ofstream("E:/git/renderserver/tmp.ida.py") << "\n";
+  hook (sub_1401F3480, sub_1401F3480_hook);
+  
   hook ( db_load_with_dbgenfallback
-       , static_cast<decltype(db_load_with_dbgenfallback)::signature*>([] (wowdbclient* a, char const* b, char const* c, unsigned int d)
+       , [] (wowdbclient* a, char const* b, char const* c, unsigned int d)
          {
-           std::cerr << "load db2 " << a->info->name << "\n";
            if (std::string (a->info->name) == "AnimationNames") return true;
            if (std::string (a->info->name) == "FileDataComplete") return true;
            if (std::string (a->info->name) == "FilePaths") return true;
            return db_load_with_dbgenfallback (a, b, c, d);
          }
-         )
        );
   hook ( db_load_with_dbgenfallback_no_override
-       , static_cast<decltype(db_load_with_dbgenfallback_no_override)::signature*> ([] (wowdbclient* a, unsigned int b)
+       , [] (wowdbclient* a, unsigned int b)
          {
-           std::cerr << "load db2 " << a->info->name << "\n";
            if (std::string (a->info->name) == "AnimationNames") return true;
            if (std::string (a->info->name) == "FileDataComplete") return true;
            if (std::string (a->info->name) == "FilePaths") return true;
            return db_load_with_dbgenfallback_no_override (a, b);
          }
-         )
        );
 
-
-  hook ( CVarRegister
-       , static_cast<CVar* (*)(char const*, char const*, unsigned int, char const*, bool (*)(CVar*, char const*, char const*, void *), unsigned int, bool, void *, bool)>
-           ( [] (char const* name, char const* a, unsigned int b, char const* c, bool (*d)(CVar*, char const*, char const*, void *), unsigned int e, bool f, void * g, bool h)
-             {
-               return CVarRegister (name, a, b, c, d, e, f, g, h);
-             }
-           )
-       );
        
+  hook ( CVarRegister
+       , [] (char const* name, char const* a, unsigned int b, char const* c, bool (*d)(CVar*, char const*, char const*, void *), unsigned int e, bool f, void * g, bool h)
+         {
+           return CVarRegister (name, a, b, c, d, e, f, g, h);
+         }
+       );
+
   hook ( add_route_a
-       , static_cast<void (*)(char const* path, blz::function<void, RenderRequest::RequestData*, route_arg_3_t*> const* fun)> 
-           ( [] (char const* path, blz::function<void, RenderRequest::RequestData*, route_arg_3_t*> const* fun)
-             {
-               std::cerr << "add route a: " << path << "\n";
-               return add_route_a (path, fun);
-             }
-           )
+       , [] (char const* path, blz::function<void, RenderRequest::RequestData*, route_arg_3_t*> const* fun)
+         {
+           std::cerr << "add route a: " << path << "\n";
+           return add_route_a (path, fun);
+         }
        );
   hook ( add_route_b
-       , static_cast<void (*)(char const* path, blz::function<void, RenderRequest::RequestData*, route_arg_3_t*> const* fun)> 
-           ( [] (char const* path, blz::function<void, RenderRequest::RequestData*, route_arg_3_t*> const* fun)
-             {
-               std::cerr << "add route b: " << path << "\n";
-               return add_route_b (path, fun);
-             }
-           )
+       , [] (char const* path, blz::function<void, RenderRequest::RequestData*, route_arg_3_t*> const* fun)
+         {
+           std::cerr << "add route b: " << path << "\n";
+           return add_route_b (path, fun);
+         }
        );
-	   
+   
+  hook ( renderservice_main
+       , []
+         {
+           add_route ("/test", &callback_test);
+           return renderservice_main();
+         }
+       );
+}
+
+
+extern "C" void __declspec(dllexport) __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO* inRemoteInfo)
+{
+  on_inject();
+  
   add_route ("/test", &callback_test);
   
   RhWakeUpProcess();
 }
+ 
